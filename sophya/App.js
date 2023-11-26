148c409efe4fb4b0e50f42e7ef4218d68199d139
sophya1234 <barcelos.sophya@gsuite.iff.edu.br>
2023-11-26 19:26:56 -0300

    inputs

--- sophya/App.js
+++ sophya/App.js
@@ -1,4 +1,6 @@
-import { View, Text, StyleSheet, Button } from "react-native";
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { View, Text, StyleSheet, Button, TextInput, Switch } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from '@react-navigation/stack'
 import { useNavigation } from "@react-navigation/native";
@@ -58,11 +60,30 @@ function TelaHome() {
 }
 
 function TelaMessages () {
-    const navigation = useNavigation()
+    const navigation = useNavigation();
+    const [text, onChangeText] = React.useState('');
     return (
       <View style={style.container}>
         <Text>messages</Text>
-      
+        <Text>chat</Text><Button />
+
+
+        <TextInput
+        placeholder="digite sua mensagem no chat"
+        keyboardType="email-address"
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+       <Text>Ative a opção para manter as mensagens no chat </Text>
+       <Switch
+        trackColor={{false: '#767577', true: '#81b0ff'}}
+        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+    
      <Button title="Home" onPress={() => {
         navigation.navigate("TelaHome")
       }}/>
@@ -85,11 +106,23 @@ function TelaMessages () {
   }
 
   function TelaSearch () {
-    const navigation = useNavigation()
+    const navigation = useNavigation();
+    const [text, onChangeText] = React.useState('');
     return (
       <View style={style.container}>
         <Text>search</Text>
-       
+        <Text>procurar</Text><Button />
+
+        <TextInput
+        placeholder="procure um produto"
+        keyboardType="email-address"
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+
+
+    
         <Button title="Home" onPress={() => {
         navigation.navigate("TelaHome")
       }}/>
@@ -118,10 +151,24 @@ function TelaMessages () {
 
 
   function TelaProfile () {
-    const navigation = useNavigation()
+    const navigation = useNavigation();
+    const [text, onChangeText] = React.useState('');
     return (
       <View style={style.container}>
         <Text>profile</Text>
+        <Text>sign in</Text><Button />
+     
+        <TextInput
+        placeholder="digite seu email"
+        keyboardType="email-address"
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <TextInput placeholder="digite sua senha" secureTextEntry={true} style={styles.input}/>
+      <StatusBar style="auto" />
+     
+
         <Button title="Home" onPress={() => {
         navigation.navigate("TelaProfile")
       }}/>
@@ -154,6 +201,15 @@ function TelaMessages () {
     return (
       <View style={style.container}>
         <Text>carinho</Text>
+
+        <Text>Ative a opção para apagar os produtos do carinho</Text>
+       <Switch
+        trackColor={{false: '#767577', true: '#81b0ff'}}
+        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
        
         <Button title="Home" onPress={() => {
         navigation.navigate("TelaHome")
@@ -177,11 +233,37 @@ function TelaMessages () {
     )
   }
   function TelaSignup () {
-    const navigation = useNavigation()
+    const navigation = useNavigation();
+    const [text, onChangeText] = React.useState('');
     return (
       <View style={style.container}>
         <Text>sign up</Text>
-       
+        <Text>sign up </Text><Button />
+        <TextInput
+        placeholder="digite seu primeiro nome"
+        keyboardType="email-address"
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+
+<TextInput
+        placeholder="digite seu sobrenome"
+        keyboardType="email-address"
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+<TextInput
+        placeholder="digite seu email"
+        keyboardType="email-address"
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      /> 
+<TextInput placeholder="digite sua senhaa" secureTextEntry={true} style={styles.input}/>
+<TextInput placeholder="redigite sua senha" secureTextEntry={true} style={styles.input}/>
+   
         <Button title="Sign up" onPress={() => {
         navigation.navigate("TelaHome")
       }}/>
@@ -193,11 +275,21 @@ function TelaMessages () {
       </View>
     )
   }  function TelaForgotpassword () {
-    const navigation = useNavigation()
+    const navigation = useNavigation();
+    const [text, onChangeText] = React.useState('');
     return (
       <View style={style.container}>
         <Text>forgot</Text>
-       
+        <Text>forgot? </Text><Button />
+
+        <TextInput
+        placeholder="digite seu email"
+        keyboardType="email-address"
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      /> 
+     
         <Button title="Home" onPress={() => {
         navigation.navigate("TelaHome")
       }}/>
@@ -246,5 +338,11 @@ const style = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   }
 })
